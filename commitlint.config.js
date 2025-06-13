905833563ff27a74b0f54466e3a29048888fbfe1
Yair Etziony <yey@medicuja.com>
2025-06-13 20:26:44 +0200

    fix: add commitlint configuration file

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,7 @@
-module.exports = {extends: ["@commitlint/config-conventional"]};
+module.exports = {
+  rules: {
+    'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore']],
+    'subject-empty': [2, 'never'],
+    'type-empty': [2, 'never']
+  }
+};
